439c6fc95427b8e448890646f1ee4fc8bf2d9e53
Evgeny Alpeyev <krotpk@gmail.com>
2021-10-11 21:11:05 +0300

    Terraform 2 replace state to backet #1

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,7 @@
-/* output "service_key_out" {
+output "service_key_out" {
   value = yandex_storage_bucket.tf-state-bucked.access_key
-} */
-/* output "service_key_secret" {
+}
+output "service_key_secret" {
   value     = yandex_storage_bucket.tf-state-bucked.secret_key
   sensitive = true
-} */
+}
